caf42eceaab6bfbd93d61e32a31d99a6297b5c00
Braden Sten <b.sten88@gmail.com>
2017-08-05 13:57:24 -0600

    created pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/inventory'
+
+  get 'pages/financing'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
