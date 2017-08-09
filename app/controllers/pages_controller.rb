8308245a55e315b59488f6222c1501de9f8390a9
Dipesh Barakoti <dbarakoti@gmail.com>
2017-08-08 18:09:15 -0600

    Implemented styles, along with seo functions for universal search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,22 @@
 class PagesController < ApplicationController
+
   def home
+   
   end
 
   def inventory
+    @page_title = "XYZ Auto-inventory"
   end
 
   def financing
+    @page_title = "XYZ Auto-Financing"
   end
 
   def contact
+    @page_title = "XYZ Auto-Contact us"
   end
 
   def about
+    @page_title = "XYZ Auto-About us"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   get 'pages/about'
 
+  get 'pages/show'
+
 end
