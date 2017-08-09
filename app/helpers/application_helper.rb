f3b823e77ddf23802010f836882a2115b5917eec
Dipesh Barakoti <dbarakoti@gmail.com>
2017-08-09 17:19:05 -0600

    Created login helper to match the style with the nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+	def login_helper style
+		if current_user
+			(link_to "Logut", destroy_user_session_path, method: :delete, class: style) + " ".html_safe +
+			
+			(link_to "Register", new_user_registration_path, class: style)
+			else
+			link_to "Login", new_user_session_path, class: style
+
+		end
+	end
 end
+
+
+
+
+# if current_user 
+#          (link_to "Logout", destroy_user_session_path, method: :delete, class: "style") + "<br>".html_safe + 
+#           else 
+#           link_to "Register", new_user_registration_path, class: "style" 
+#           link_to "Login", new_user_session_path,  class: "style" 
+#       end
+# 	end
